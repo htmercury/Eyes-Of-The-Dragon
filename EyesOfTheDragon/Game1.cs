7066132c8617df32d21dba4ae060db090b0ce5fb
Ka Wong <kawong2020@u.northwestern.edu>
2017-09-19 10:33:56 -0500

    Updated Game1 constructor to create a TitleScreen State and had the state manager set the title screen as the first state

--- EyesOfTheDragon/Game1.cs
+++ EyesOfTheDragon/Game1.cs
@@ -1,37 +1,63 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 using XRpgLibrary;
+using EyesOfTheDragon.GameScreens;
 
 namespace EyesOfTheDragon
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class Game1 : Game
     {
+        #region XNA Field region
+
         GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        public SpriteBatch spriteBatch;
+        #endregion
+
+        #region Game State region
+
         GameStateManager stateManager;
+        public TitleScreen TitleScreen;
+        #endregion
+
+        #region Screen Field region
+        const int screenWidth = 1024;
+        const int screenHeight = 768;
+
+        public readonly Rectangle ScreenRectangle;
+        #endregion
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = screenWidth;
+            graphics.PreferredBackBufferHeight = screenHeight;
+
+            ScreenRectangle = new Rectangle(
+                0,
+                0,
+                screenWidth,
+                screenHeight);
+
             Content.RootDirectory = "Content";
 
             Components.Add(new InputHandler(this));
 
             stateManager = new GameStateManager(this);
             Components.Add(stateManager);
+
+            TitleScreen = new TitleScreen(this, stateManager);
+            stateManager.ChangeState(TitleScreen);
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
@@ -39,10 +65,6 @@ namespace EyesOfTheDragon
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
@@ -51,10 +73,6 @@ namespace EyesOfTheDragon
             // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
@@ -75,10 +93,6 @@ namespace EyesOfTheDragon
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
